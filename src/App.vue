6daf001811b06f91b73101dff743c234e6101a13
luluHyggeWOW <1344085034@qq.com>
2023-06-15 15:53:42 +0800

    测试

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <h1>APP根组件</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 2px;
+  h1 {
+    color: wheat;
+  }
+}
+</style>
